6ba3e551b3cdbc369ccc36c40dfd23311c2a2d20
Paulo Henrique Estevam <paulohenriqueestevam2@gmail.com>
2025-06-13 16:25:34 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,19 @@
+def inverter(dado):
+    lista_i.append(dado);
+    lista_i.sort(reverse=True);
+
+def reverter(dado):
+    invertido = int(str(dado)[::-1]);
+    lista_r.append(invertido);
+
+limite = int(input("Digite quantos números serão inseridos: "))
+lista_i = [];
+lista_r = [];
+
+for i in range(limite):
+    dado = int(input("Digite o número que entrará na lista: "));
+    inverter(dado);
+    reverter(dado);
+
+print(f"Lista em ordem decrescente: {lista_i}");
+print(f"Lista com números invertidos: {lista_r}");
